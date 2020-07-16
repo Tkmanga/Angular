713a539fb6ed199fb8a6abd27a7d2ed007fddca5
Tkmanga <tacachojose@gmail.com>
2020-07-16 17:24:47 -0300

    udemy

--- Maquetacion/Backend/app.js
+++ Maquetacion/Backend/app.js
@@ -15,7 +15,14 @@ var article_routes = require('./routes/article');
 app.use(bodyParse.urlencoded({extended:false}));
 app.use(bodyParse.json());
 //CORS
-
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 //aniadir prefijo a rutas
 app.use('/api',article_routes);
 //ruta de prueba para el api rest
