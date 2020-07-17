375d85d6f5af0271cf97fbc769242d7a7dacbedd
Tkmanga <tacachojose@gmail.com>
2020-07-17 13:48:26 -0300

    udemy

--- Maquetacion/AprendiendoAngular/src/app/components/mi-componente/mi-componente.component.ts
+++ Maquetacion/AprendiendoAngular/src/app/components/mi-componente/mi-componente.component.ts
@@ -7,14 +7,20 @@ export class MiComponente{
   public titulo: string;
   public comentario: string;
   public year: number;
+  public mostrarPeliculas: boolean;
 
   constructor() {
     this.titulo = "Hola mundo, soy mi componente";
     this.comentario = "Este es mi primer comentario ";
     this.year = 2020;
+    this.mostrarPeliculas = true;
+
 
     console.log('mi componente cargado');
   }
 
+  ocultarPeliculas(){
+    this.mostrarPeliculas = false;
 
+  }
 }
--- Maquetacion/AprendiendoAngular/src/app/components/peliculas/peliculas.component.ts
+++ Maquetacion/AprendiendoAngular/src/app/components/peliculas/peliculas.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-peliculas',
   templateUrl: './peliculas.component.html',
   styleUrls: ['./peliculas.component.css']
 })
-export class PeliculasComponent implements OnInit {
 
-  constructor() { }
+//OnInit es un hook
+
+export class PeliculasComponent implements OnInit, DoCheck {
+  public titulo: string;
+
+  constructor() {
+    this.titulo = "Componente peliculas";
+    console.log('constructor lanzado'); //esto seria lo primero q se ejecuta
+
+  }
 
   ngOnInit(): void {
+
+    console.log('componente iniciado!!'); // despues de ejecuta esto
   }
+  //el metodo doche se ejecuta cada vez que hay un cambio en el componente
+  ngOnDestroy() {
+    console.log('el componente se va eliminar')
+  }
+  ngDoCheck() {
+    console.log('DoCheck Lanzado ');
+  }
+
+  cambiarTitulo (){
+    this.titulo = "El titulo ah sido cambiado";
+  }
+
 
 }
