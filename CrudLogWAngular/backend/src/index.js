c2cc4dc947c8c48b03ec18fe3e20a29bbc67f226
Tkmanga <tacachojose@gmail.com>
2020-08-18 20:08:30 -0300

    CRUD

--- CrudLogWAngular/backend/src/index.js
+++ CrudLogWAngular/backend/src/index.js
@@ -1,17 +1,30 @@
 //inicio
 const exp = require('express');
 const  app  = exp();
-const exhbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const expSession = require('express-session');
 //setting
 app.set('port', process.env.PORT || 3000);
 app.set('views',path.join(__dirname,'views'));
-/*app.engine('.hbs',exhbs({
+app.engine('.hbs',exphbs({
     defaultLayout: 'main',
-    layoutsDir: path.join(),
-    partialsDir: ,
-    extname:
-}));*/
+    layoutsDir: path.join(app.get('views'),'layouts'),
+    partialsDir: path.join(app.get('views'),'partials'),
+    extname: '.hbs'
+}));
+
+app.set('view engine','.hbs');
 //middlewares
+app.use(exp.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
+app.use(expSession({
+    secret: 'boldt',
+    resave: true,
+    saveUninitialized: true
+}));
+
+
 //global variables
 //Routes
 //static files
